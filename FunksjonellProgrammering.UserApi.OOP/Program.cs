4ac3329e710d9c65e1f4f7dafb609b8456da20ac
Kim Rune Solstad <krs@itverket.no>
2023-11-12 08:36:12 +0100

    Constants i egen fil

--- FunksjonellProgrammering.UserApi.OOP/Program.cs
+++ FunksjonellProgrammering.UserApi.OOP/Program.cs
@@ -5,8 +5,10 @@ global using static LaYumba.Functional.F;
 using Dapper;
 using FunksjonellProgrammering.Shared;
 using FunksjonellProgrammering.Shared.Primitives;
-using FunksjonellProgrammering.UserApi.OOP.Repositories;
-using Microsoft.AspNetCore.Http.Json;
+using FunksjonellProgrammering.UserApi.OOP;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using JsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
 
 SqlMapper.AddTypeHandler(new NameTypeHandler());
 SqlMapper.AddTypeHandler(new RoleTypeHandler());
@@ -16,7 +18,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<InitDb>();
-builder.Services.AddSingleton<IUserRepository, UserRepository>();
 builder.Services.AddSingleton<ConnectionString>();
 builder.Services.Configure<JsonOptions>(options =>
 {
@@ -62,5 +63,61 @@ app.UseAuthorization();
 app.MapControllers();
 
 var connectionString = app.Services.GetRequiredService<ConnectionString>();
+
+Func<UserId, Exceptional<Option<User>>> ConfigureRead(ConnectionString connectionString)
+=> id =>
+{
+    try
+    {
+        return connectionString.Connect(c => c
+            .Query(Constants.ReadSql, new {Id = (int) id})
+            .Select(x => new User(x.Name, x.Role))
+            .Match(
+                Empty: () => None,
+                Otherwise: (user, _) => Some(user)
+            )
+        );
+    }
+    catch (Exception e)
+    {
+        return e;
+    }
+};
+
+var read = ConfigureRead(connectionString);
+
+app.MapGet("/user/{id:int}", (int id)
+=> read(id)
+    .Match(
+        Exception: _ => Results.Problem(),
+        Success: u => u
+            .Match(
+                None: () => Results.NotFound(),
+                Some: Results.Ok
+)));
+
+Func<User, Exceptional<Unit>> ConfigureCreate(ConnectionString connectionString)
+=> user =>
+{
+    try
+    {
+        connectionString.Connect(c => c.Execute(Constants.CreateSql, user));
+    }
+    catch (Exception e)
+    {
+        return e;
+    }
+
+    return Unit();
+};
+
+var create = ConfigureCreate(connectionString);
+
+app.MapPost("/user", (User user)
+=> create(user)
+    .Match(
+        Exception: _ => Results.Problem(),
+        Success: _ => Results.Created("/user/", user)
+));
 
 app.Run();
